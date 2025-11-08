d8ae701c556bd61d191b76e573d0a2d3740f24cf
Ken Tabata <kntabataba@icloud.com>
2025-11-07 21:02:46 -0600

    Saad is genius

--- Trick_Click/__init__.py
+++ Trick_Click/__init__.py
@@ -3,12 +3,20 @@ import os
 
 def create_app():
 
-    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Trick or Click"))
+    frontend_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../Trick or Click"))
 
-    app = Flask(__name__, template_folder=template_dir)
+    app = Flask(
+    __name__,
+    template_folder=frontend_dir,
+    static_folder=frontend_dir      
+    )
+
     app.config['SECRET_KEY'] = 'your-secret-key'
 
     from .routes import bp as routes_bp
     app.register_blueprint(routes_bp)
+
+    print("  template_folder =", app.template_folder)
+    print("Static folder:", app.static_folder)
     
     return app
